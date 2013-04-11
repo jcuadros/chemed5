5baf23287bfb4f0c33b1447d40a0964f640d084c
jaume.sanbor@gmail.com <jaume.sanbor@gmail.com@16d59760-3bf3-e48b-4e41-b9a82e644877>
2013-04-11 17:28:41 +0000

    recol·locades les fletxes de canviar de pàgina i agregat una tercera fletxa per pujar de nivell. Bloqueig dels botons de resposta que no s'està visualitzant. Bloqueig total dels botons quan s'escull una resposta. Arreglat el problema del modal de l'ajuda i retirat el fons ennegrit d'aquest. /Seetings ignorats.

--- trunk/jscript/mainjs.js
+++ trunk/jscript/mainjs.js
@@ -1,18 +1,72 @@
+function overlay() {
+	el = document.getElementById("ayuda");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
 
+document.getElementById("op1").onclick= function(){
+	document.getElementById("r1").disabled=false;
+	document.getElementById("r2").disabled=true;
+	document.getElementById("r3").disabled=true;
+	document.getElementById("r4").disabled=true;
+};
 
-function showHideA(){
-	var ImageDiv=document.getElementById('img1div');
-	ImageDiv.style.display=(ImageDiv.style.display=='none')?'inline':'none';}
+document.getElementById("op2").onclick= function(){
+	document.getElementById("r1").disabled=true;
+	document.getElementById("r2").disabled=false;
+	document.getElementById("r3").disabled=true;
+	document.getElementById("r4").disabled=true;
+};
 
-function showHideB(){
-	var ImageDiv=document.getElementById('img2div');
-	ImageDiv.style.display=(ImageDiv.style.display=='none')?'inline':'none';}
+document.getElementById("op3").onclick= function(){
+	document.getElementById("r1").disabled=true;
+	document.getElementById("r2").disabled=true;
+	document.getElementById("r3").disabled=false;
+	document.getElementById("r4").disabled=true;
+};
 
-function showHideC(){
-	var ImageDiv=document.getElementById('img3div');
-	ImageDiv.style.display=(ImageDiv.style.display=='none')?'inline':'none';}
+document.getElementById("op4").onclick= function(){
+	document.getElementById("r1").disabled=true;
+	document.getElementById("r2").disabled=true;
+	document.getElementById("r3").disabled=true;
+	document.getElementById("r4").disabled=false;
+};
 
-function showHideD(){
-	var ImageDiv=document.getElementById('img4div');
-	ImageDiv.style.display=(ImageDiv.style.display=='none')?'inline':'none';}
+document.getElementById("r1").onclick= function(){
+	var nodes = document.getElementById("resp").getElementsByTagName('*');
+	for(var i = 0; i < nodes.length; i++)
+	{
+	     nodes[i].disabled = true;
+	}
+};
 
+document.getElementById("r1").onclick= function(){
+	var nodes = document.getElementById("resp").getElementsByTagName('*');
+	for(var i = 0; i < nodes.length; i++)
+	{
+	     nodes[i].disabled = true;
+	}
+};
+
+document.getElementById("r2").onclick= function(){
+	var nodes = document.getElementById("resp").getElementsByTagName('*');
+	for(var i = 0; i < nodes.length; i++)
+	{
+	     nodes[i].disabled = true;
+	}
+};
+
+document.getElementById("r3").onclick= function(){
+	var nodes = document.getElementById("resp").getElementsByTagName('*');
+	for(var i = 0; i < nodes.length; i++)
+	{
+	     nodes[i].disabled = true;
+	}
+};
+
+document.getElementById("r4").onclick= function(){
+	var nodes = document.getElementById("resp").getElementsByTagName('*');
+	for(var i = 0; i < nodes.length; i++)
+	{
+	     nodes[i].disabled = true;
+	}
+};
