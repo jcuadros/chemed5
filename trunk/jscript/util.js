da435dc9e30481609e3fcc6c0683e0832da29df5
jaume.sanbor@gmail.com <jaume.sanbor@gmail.com@16d59760-3bf3-e48b-4e41-b9a82e644877>
2013-04-26 18:24:25 +0000

    Canviats els documents de Jmol pels nous documents proporcionats per l'Angel. Afegides les etiquetes de codificació utf-8 a les pàgines. actualitzat el document util.js Eliminat el document Jmol.js, la línea que el cridava; i afegida la línea que crida al nou script JSmol.min.js

--- trunk/jscript/util.js
+++ trunk/jscript/util.js
@@ -1,19 +1,29 @@
+
+var JmolInfo = {
+  color: "black",
+  height: 320,
+  width: 320,
+  j2sPath: "Jmol/j2s",
+  jarPath: "Jmol/java",
+  isSigned: true,
+  jarFile: "JmolAppletSigned0.jar",
+  use: "Java"
+};
+
 var path = document.location.href;
 var k1 = path.lastIndexOf('/');
 path = path.substring(0,k1);
 var scr0 = 'zap; set defaultDirectory "'+ path + '"; '+
-  'set echo warn 50% 50%; set echo warn center; echo "loading..."; refresh;';
+  'set echo warn 50% 50%; set echo warn center; echo "cargando..."; refresh;';
 
 function cargaMol(x) {
-	jmolScript(scr0 + 'load '+x);
+	Jmol.script(myJmol, scr0 + 'load '+x);
 }
 function cargaScript(x) {
 	if (x.length==0) {
-		jmolScript('zap;');
+		Jmol.script(myJmol, 'zap;');
 	} else {
 		//alert(scr0 + 'script '+x);
-		jmolScript(scr0 + 'script '+x);
+		Jmol.script(myJmol, scr0 + 'script "' + x + '";');
 	}
 }
-
-function gid(x) { return document.getElementById(x); }
